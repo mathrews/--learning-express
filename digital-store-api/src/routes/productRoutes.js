478f715ed5352ec0c1b3ff704e9832d2a92fc8c0
mathrews <mateusps4gg@gmail.com>
2023-12-28 12:22:54 -0300

    cleaning operation

--- digital-store-api/src/routes/productRoutes.js
+++ digital-store-api/src/routes/productRoutes.js
@@ -3,13 +3,6 @@ const router = express.Router();
 
 const controller = require("../controllers/productController")
 
-function bigIntToJSON(key, value) {
-  if (typeof value === 'bigint') {
-    return value.toString();
-  }
-  return value;
-}
-
 router.get("/", async (req, res) => {
   res.send(await controller.listAll());
 });
