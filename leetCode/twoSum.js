21eb8ec770e52117f521ecddcc5c06b75d81381d
mathrews <mateusps4gg@gmail.com>
2024-01-06 08:26:46 -0300

    twosum going

--- leetCode/twoSum.js
+++ leetCode/twoSum.js
@@ -1,19 +1,25 @@
-var twoSum = function(nums, target) {
-    let index1 = 0
-    let index2 = 1
+var twoSum = function (nums, target) {
+    let index1 = 0;
+    let index2 = 1;
     for (let i = 0; i < nums.length; i++) {
         if (nums[index1] + nums[index2] != target) {
-            index1++;
-            index2++;
-        } else {
-            return [nums.indexOf(nums[index1]), nums.indexOf(nums[index2])]
-            // if (nums[index1] == nums[index2]) {
+            nums.forEach((iten, index) => {
                 
-            // } else {
-            //     return [nums.indexOf(nums[index1]), nums.indexOf(nums[index2])]
-            // }
+            })
+        } else {
+            if (nums.indexOf(nums[index1]) == nums.indexOf(nums[index2])) {
+                let indices = [];
+                nums.filter(function (iten, index) {
+                    if (iten == nums[index2]) {
+                        indices.push(index);
+                    }
+                });
+                return [nums.indexOf(nums[index1]), indices[1]];
+            } else {
+                return [nums.indexOf(nums[index1]), nums.indexOf(nums[index2])];
+            }
         }
-    } 
+    }
 };
 
-console.log(twoSum([3, 2, 3], 6));
+console.log(twoSum([-1,-2,-3,-4,-5], -8));
